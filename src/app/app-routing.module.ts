821a33d1627bce3c6e1618a7a613baea84f46e66
Adrianus Rian <adrianus.rian2@gmail.com>
2021-12-03 20:48:51 +0700

    udah selesai

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { ScoringComponent } from './scoring/scoring.component';
+import { StandingComponent } from './standing/standing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'admin',component:AdminComponent},
+  {path:'scoring',component:ScoringComponent},
+  {path:'standing',component:StandingComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { ScoringComponent } from './scoring/scoring.component';
+import { StandingComponent } from './standing/standing.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ScoringComponent,
+    StandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
